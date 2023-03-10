6111979f96612f304768fcedf23f93de8aa0f20b
Tom McCollough <tom.mccollough@gmail.com>
2023-03-10 05:36:34 -0700

    focus

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,13 +4,15 @@ function Banner() {
   return (
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
       <div>
+      <h1 className="text-3xl mb-8">Hello World, For Real</h1>
         <p className="font-normal text-2xl mb-3">Hi, I am</p>
-        <h1 className="text-5xl">W. Thomas McCollough</h1>
+        <h1 className="text-5xl">Tom McCollough</h1>
         <h2 className="mt-5 md:mt-2 text-xl font-normal">Product and Technology Leadership for Developer Solutions</h2>
         <div className="mt-4 mb-10 text-base font-normal">
           <p className="mt-3">I am a technology leader with considerable experience building and running product
           management and engineering teams. With a computer science background, I focus on serving developers with
-          cloud-native managed services.</p>
+          cloud-native managed services, with a present focus on <span className="font-bold">helping developers write global
+          software just as easily as they wrote their first "Hello World".</span></p>
           <p className="mt-3">Able to serve in product roles at the principal or director level,
           or as director-level leadership overseeing both product+engineering.</p>
           <p className="mt-3">I'm able to contribute in either the pre-product market fit phase or
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
         <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5">
             <div className="flex items-center space-x-2">
                 <h1><Link href="/">Home</Link></h1>
-                <h1 className="inline-flex items-center px-6"><Link href="/blog">Blog</Link></h1>
+                {/* <h1 className="inline-flex items-center px-6"><Link href="/blog">Blog</Link></h1> */}
             </div>
             <div>
                 <SocialIcon className="mr-2" target="_blank" style={{ height: 35, width: 35 }} url="https://linkedin.com/in/wtmccollough" />
