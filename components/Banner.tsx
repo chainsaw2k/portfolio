854795428c49162f4ede4c76e80c3a3ed9c0d888
Tom McCollough <tom.mccollough@gmail.com>
2023-03-10 13:18:43 -0700

    chg

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
       <div>
-      <h1 className="text-3xl mb-8">Hello World, For Real</h1>
+      <h1 className="text-3xl mb-8">"Hello World", For Real</h1>
         <p className="font-normal text-2xl mb-3">Hi, I am</p>
         <h1 className="text-5xl">Tom McCollough</h1>
         <h2 className="mt-5 md:mt-2 text-xl font-normal">Product and Technology Leadership for Developer Solutions</h2>
